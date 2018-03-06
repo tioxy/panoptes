b7cef2ac2329619511ad19b20f1b82d0a7f89d8e
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-03-06 20:50:09 -0300

    Adding analyze commands linux help

--- panoptes/panoptes.py
+++ panoptes/panoptes.py
@@ -5,32 +5,58 @@ import cloud_providers
 
 @click.group()
 def cli():
+    """Welcome to Panoptes - The multi cloud security group analyzer
+
+    This project is stored at GitHub and open sourced under the Apache 2.0 License.
+
+    To start using it, read the docs at:
+    https://github.com/tioxy/panoptes
+    """
     pass
 
 
-@cli.group('aws')
+@cli.group(
+    'aws',
+    help='Amazon Web Services'
+)
 def aws():
     pass
 
 
-@aws.command('analyze')
+@cli.group(
+    'gcp',
+    help='Google Cloud Plataform'
+)
+def gcp():
+    pass
+
+
+@aws.command(
+    'analyze',
+    help="Generate the analysis file"
+)
 @click.option(
     '--region',
-    help='AWS Region to check your security groups'
+    required=True,
+    help='AWS Region to list the security groups',
+    metavar='<region_id>'
 )
 @click.option(
     '--profile',
     default='default',
-    help='AWS CLI profile to check'
+    help='AWS CLI configured profile which will be used',
+    metavar='<profile_name>'
 )
 @click.option(
     '--whitelist',
-    help='Whitelist to declare safe IPs'
+    help='Whitelist to declare safe IPs',
+    metavar='<path>'
 )
 @click.option(
     '--output',
     default='json',
-    help='Which kind of output you want the analysis'
+    help='Which kind of output you want the analysis',
+    metavar='<json/yml>'
 )
 def aws_analyze_command(region, profile, output, whitelist=None):
     if whitelist is None:
@@ -52,91 +78,6 @@ def aws_analyze_command(region, profile, output, whitelist=None):
             whitelist_file,
         )
     return None
-
-
-@aws.command('remove-anywhere')
-@click.option(
-    '--region',
-    help='AWS Region to check your security groups'
-)
-@click.option(
-    '--profile',
-    default='default',
-    help='AWS CLI profile to check'
-)
-@click.option(
-    '--analysis-file',
-    help='File created from the analyze command'
-)
-def aws_remove_anywhere_command(region, profile, analysis_file):
-    aws_authentication = cloud_authentication.aws.get_client(
-        region=region,
-        profile=profile,
-    )
-
-    if aws_authentication["Error"] is not None:
-        print(aws_authetication["Error"])
-    else:
-        aws_client = aws_authentication["Client"]
-        print("Still being developed")
-    return None
-
-
-@aws.command('remove-unsafe')
-@click.option(
-    '--region',
-    help='AWS Region to check your security groups'
-)
-@click.option(
-    '--profile',
-    default='default',
-    help='AWS CLI profile to check'
-)
-@click.option(
-    '--analysis-file',
-    help='File created from the analyze command'
-)
-def aws_remove_unsafe_command(region, profile, analysis_file):
-    aws_authentication = cloud_authentication.aws.get_client(
-        region=region,
-        profile=profile,
-    )
-
-    if aws_authentication["Error"] is not None:
-        print(aws_authetication["Error"])
-    else:
-        aws_client = aws_authentication["Client"]
-        print("Still being developed")
-    return None
-
-
-@aws.command('remove-unused')
-@click.option(
-    '--region',
-    help='AWS Region to check your security groups'
-)
-@click.option(
-    '--profile',
-    default='default',
-    help='AWS CLI profile to check'
-)
-@click.option(
-    '--analysis-file',
-    help='File created from the analyze command'
-)
-def aws_remove_unsafe_command(region, profile, analysis_file):
-    aws_authentication = cloud_authentication.aws.get_client(
-        region=region,
-        profile=profile,
-    )
-
-    if aws_authentication["Error"] is not None:
-        print(aws_authetication["Error"])
-    else:
-        aws_client = aws_authentication["Client"]
-        print("Still being developed")
-    return None
-   
 
 
 def read_whitelist_file(whitelist):
