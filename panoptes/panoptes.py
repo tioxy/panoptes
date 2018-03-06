5f0e7cdff5f29e6a8a44d3646cd95eabe2d742ff
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-03-06 20:08:30 -0300

    Adding new commands skeleton to panoptes

--- panoptes/panoptes.py
+++ panoptes/panoptes.py
@@ -8,7 +8,12 @@ def cli():
     pass
 
 
-@cli.command('aws')
+@cli.group('aws')
+def aws():
+    pass
+
+
+@aws.command('analyze')
 @click.option(
     '--region',
     help='AWS Region to check your security groups'
@@ -27,7 +32,7 @@ def cli():
     default='json',
     help='Which kind of output you want the analysis'
 )
-def aws_command(region, profile, output, whitelist=None):
+def aws_analyze_command(region, profile, output, whitelist=None):
     if whitelist is None:
         whitelist_file = None
     else:
@@ -47,6 +52,91 @@ def aws_command(region, profile, output, whitelist=None):
             whitelist_file,
         )
     return None
+
+
+@aws.command('remove-anywhere')
+@click.option(
+    '--region',
+    help='AWS Region to check your security groups'
+)
+@click.option(
+    '--profile',
+    default='default',
+    help='AWS CLI profile to check'
+)
+@click.option(
+    '--analysis-file',
+    help='File created from the analyze command'
+)
+def aws_remove_anywhere_command(region, profile, analysis_file):
+    aws_authentication = cloud_authentication.aws.get_client(
+        region=region,
+        profile=profile,
+    )
+
+    if aws_authentication["Error"] is not None:
+        print(aws_authetication["Error"])
+    else:
+        aws_client = aws_authentication["Client"]
+        print("Still being developed")
+    return None
+
+
+@aws.command('remove-unsafe')
+@click.option(
+    '--region',
+    help='AWS Region to check your security groups'
+)
+@click.option(
+    '--profile',
+    default='default',
+    help='AWS CLI profile to check'
+)
+@click.option(
+    '--analysis-file',
+    help='File created from the analyze command'
+)
+def aws_remove_unsafe_command(region, profile, analysis_file):
+    aws_authentication = cloud_authentication.aws.get_client(
+        region=region,
+        profile=profile,
+    )
+
+    if aws_authentication["Error"] is not None:
+        print(aws_authetication["Error"])
+    else:
+        aws_client = aws_authentication["Client"]
+        print("Still being developed")
+    return None
+
+
+@aws.command('remove-unused')
+@click.option(
+    '--region',
+    help='AWS Region to check your security groups'
+)
+@click.option(
+    '--profile',
+    default='default',
+    help='AWS CLI profile to check'
+)
+@click.option(
+    '--analysis-file',
+    help='File created from the analyze command'
+)
+def aws_remove_unsafe_command(region, profile, analysis_file):
+    aws_authentication = cloud_authentication.aws.get_client(
+        region=region,
+        profile=profile,
+    )
+
+    if aws_authentication["Error"] is not None:
+        print(aws_authetication["Error"])
+    else:
+        aws_client = aws_authentication["Client"]
+        print("Still being developed")
+    return None
+   
 
 
 def read_whitelist_file(whitelist):
