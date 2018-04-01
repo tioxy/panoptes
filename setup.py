c714700822fee25c17f4acb93bb683e4be55cf02
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-04-01 19:27:19 -0300

    Adding sample index.md from README and updating version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='panoptes',
-    version='0.1.0',
+    version='0.1.1',
     author='Gabriel Tiossi',
     author_email='gabrieltiossi@gmail.com',
     packages=find_packages(),
