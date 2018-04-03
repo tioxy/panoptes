01ff7e640c71337e0dc70f172cf5bf1d92180e27
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-04-03 01:44:20 -0300

    Creating new setup.py release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='panoptes',
-    version='0.1.1',
+    version='0.2.0',
     author='Gabriel Tiossi',
     author_email='gabrieltiossi@gmail.com',
     packages=find_packages(),
