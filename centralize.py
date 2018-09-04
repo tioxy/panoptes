60bcca284f65583dc5ff180a265efb89ad28cdc7
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-09-04 19:55:01 -0300

    Removing unused centralize file

--- centralize.py
+++
@@ -1,8 +0,0 @@
-
-
-if __name__ == "__main__":
-    base_string = "==============================================="
-    content = input("Digite a mensagem")
-    print(base_string)
-    print(centralize_content_from_message(base_string, content))
-    print(base_string)
