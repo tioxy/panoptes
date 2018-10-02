9183d208529fd33d9eeb99d7af7cf281853365a3
Gabriel Tiossi <gabrieltiossi@gmail.com>
2018-10-02 01:25:32 -0300

    Adding get_boto_clients, sample type hints in aws/authentication.py

--- panoptes/aws/authentication.py
+++ panoptes/aws/authentication.py
@@ -4,32 +4,50 @@ Using AWS best practices forcing the user to write Named Profiles instead of
 direct IAM credentials through CLI.
 """
 
-import boto3
 import panoptes.generic.exceptions
+import boto3
 
 
-def create_session(region, profile=None, session_token=None):
+def create_session(
+        region: str,
+        profile: str=None,
+        session_token: str=None) -> boto3.session.Session:
     """
     Generates a Boto3 session from named profile and region inputs
     """
-    aws_session = boto3.Session(
+    session = boto3.Session(
         profile_name=profile,
         region_name=region,
         aws_session_token=session_token,
     )
-    if not aws_session.get_credentials():
+    if not session.get_credentials():
         raise panoptes.generic.exceptions.PanoptesAuthError(
             "Panoptes could not authenticate to AWS. "
             "Check if your credentials exist and work."
         )
-    return aws_session
+    return session
 
 
-def get_session_info(aws_session):
+def get_session_info(session: boto3.session.Session) -> str:
     """
     Get ARN from the current session
     """
-    return aws_session.client('sts').get_caller_identity()['Arn']
+    return session.client('sts').get_caller_identity()['Arn']
+
+
+def get_boto_clients(session: boto3.session.Session) -> dict:
+    """
+    Receives the session, and return all Panoptes used Boto3 Clients
+    """
+    return {
+        'ec2': session.client('ec2'),
+        'rds': session.client('rds'),
+        'elb': session.client('elb'),
+        'elbv2': session.client('elbv2'),
+        'lambda': session.client('lambda'),
+        'elasticache': session.client('elasticache'),
+        'ecs': session.client('ecs'),
+    }
 
 
 if __name__ == "__main__":
